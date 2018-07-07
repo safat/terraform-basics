a5e6c8442e11abf08fa372f4de340d22011472e1
Muhammad Shakhawat Hossain Safat <safat.hossain@grabtaxi.com>
2018-07-08 00:52:05 +0800

    Initial setup with ASG

--- main.tf
+++ main.tf
@@ -1,13 +1,118 @@
 provider aws {
     region = "ap-southeast-1"
-
 }
 
+resource "aws_launch_configuration" "upr-launch-config-1" {
+    image_id = "ami-81cefcfd"
+    instance_type = "t2.micro"
+    security_groups = ["${aws_security_group.upr1-example-sg.id}"]
+    
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello World" > index.html
+                nohup busybox httpd -f -p "${var.server_port}" &
+                EOF
+    
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+/*
 resource "aws_instance" "upr-1" {
     ami = "ami-81cefcfd"
     instance_type = "t2.micro"
+    vpc_security_group_ids = ["${aws_security_group.upr1-example-sg.id}"]    
 
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello World" > index.html
+                nohup busybox httpd -f -p "${var.server_port}" &
+                EOF
      tags {
-        Name = "upr-1-example"
+        name = "upr-1-example"
     }
+}
+*/
+
+resource "aws_security_group" "upr1-example-sg" {
+    name = "terraform_example_instance"
+
+    ingress {
+        from_port = "${var.server_port}"
+        to_port = "${var.server_port}"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "upr1-autoscaling-group" {
+    launch_configuration = "${aws_launch_configuration.upr-launch-config-1.id}"
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    load_balancers = ["${aws_elb.upr1-elb.name}"]
+    health_check_type = "ELB"
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key = "Name"
+        value = "upr1-autoscaling-group"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_elb" "upr1-elb" {
+    name = "upr1-elb"
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    security_groups = ["${aws_security_group.upr1-elb-sg.id}"]
+
+    listener {
+        lb_port = 80
+        lb_protocol = "http"
+        instance_port = "${var.server_port}"
+        instance_protocol = "http"
+    }
+
+    health_check {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 3
+        interval = 30
+        target = "HTTP:${var.server_port}/"
+    }
+}
+
+resource "aws_security_group" "upr1-elb-sg" {
+    name = "upr1-elb-sg"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+data "aws_availability_zones" "all" {
+}
+
+variable "server_port" {
+    description = "The port that server binds to"
+    default = 8080
+}
+
+output "elb_dns_name" {
+    value = "${aws_elb.upr1-elb.dns_name}"
 }
