95e3d4552c87c97b8068fff1c0e721acfa5289b1
Muhammad Shakhawat Hossain Safat <safat.hossain@grabtaxi.com>
2018-07-08 21:19:16 +0800

    Externalise app server setup script

--- stg/webserver/main.tf
+++ stg/webserver/main.tf
@@ -12,18 +12,22 @@ data "terraform_remote_state" "db" {
   }
 }
 
+data "template_file" "user_data" {
+  template = "${file("script/server.sh")}"
+
+  vars {
+    server_port = "${var.server_port}"
+    db_address = "${data.terraform_remote_state.db.db_address}"
+  }
+}
+
 resource "aws_launch_configuration" "upr-launch-config-1" {
   image_id = "ami-81cefcfd"
   instance_type = "t2.micro"
   security_groups = [
     "${aws_security_group.upr1-example-sg.id}"]
 
-  user_data = <<-EOF
-                #!/bin/bash
-                echo "Hello World" > index.html
-                echo "${data.terraform_remote_state.db.db_address}" >> index.html
-                nohup busybox httpd -f -p "${var.server_port}" &
-                EOF
+  user_data = "${data.template_file.user_data.rendered}"
 
   lifecycle {
     create_before_destroy = true
